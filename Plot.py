a61bc9c33ab30faf9437a8e6b848551cc807ca7f
Aidne7 <89154498+Aidne7@users.noreply.github.com>
2024-04-16 16:47:21 -0400

    Swapped Variable and color scheme

--- Plot.py
+++ Plot.py
@@ -1,11 +1,22 @@
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
+import matplotlib.ticker as ticker
 from matplotlib.colors import LinearSegmentedColormap
 from matplotlib.patheffects import withStroke
 
+rgb_file = "MPL_BuPu.rgb"
+
+with open(rgb_file, 'r') as f:
+    lines = f.readlines()
+    colors = []
+    for line in lines:
+        if line.startswith('#') or line.startswith('ncolors='):
+            continue
+        colors.append([float(value) for value in line.split()])
+
 
 # Function to plot the degree days chart
-def plot_departure_map(sf, ddTable, column_name, colors, title, date_range):
+def plot_departure_map(sf, ddTable, column_name, title, date_range):
     # Creates a new table with only the columns that are needed
     newTable = ddTable[['cd', column_name]].copy()
 
@@ -18,21 +29,25 @@ def plot_departure_map(sf, ddTable, column_name, colors, title, date_range):
     # Merges the newTable with the shapefile
     mergedSF = sf.merge(newTable, on="CLIMDIV", how="left")
 
-    mergedSF[column_name] = mergedSF[column_name].apply(lambda x: 100 if x == 0 else x)
     # Calculate vmin as the minimum value in the data or -200 (whichever is smaller)
-    vmin = 50
-    # Calculate vmax as the maximum value in the data or 200 (whichever is greater)
-    vmax = 150
+    vmin = mergedSF[column_name].min()
+    # Calculate vmax as the maximum  value in the data or 200 (whichever is greater)
+    vmax = mergedSF[column_name].max()
 
+    vcenter = ((vmax - vmin) / 2) + vmin
+
+    print(vmax)
+    print(vcenter)
+    print(vmin)
     # Create a colormap with white at the center for 0
-    cmap = LinearSegmentedColormap.from_list("Custom", colors, N=256)
+    cmap = LinearSegmentedColormap.from_list("Custom", colors, N=128)
 
     # Sets the conditions for the chart
-    norm = mcolors.TwoSlopeNorm(vmin=vmin, vcenter=100, vmax=vmax)
+    norm = mcolors.TwoSlopeNorm(vmin=vmin, vcenter=vcenter, vmax=vmax)
     fig, ax = plt.subplots(1, 1)
 
     # Plots the chart with black edges
-    mergedSF.plot(column=column_name, cmap=cmap, norm=norm, linewidth=0.8, ax=ax, edgecolor="black", legend=True)
+    mergedSF.plot(column=column_name, cmap=cmap, norm=norm, linewidth=0.8, ax=ax, edgecolor="black", legend=False)
 
     # Adds text labels for each CLIMDIV section with bigger white font
     for idx, row in mergedSF.iterrows():
@@ -44,13 +59,23 @@ def plot_departure_map(sf, ddTable, column_name, colors, title, date_range):
                     fontsize=12, color='white',
                     path_effects=[withStroke(linewidth=2, foreground='black')])  # Add black outline
 
-            plt.title(title)  # Adds the title
     ax.axis('off')  # Disables the axis
 
-    #Adds the date Range
+    # Adds the date Range
     ax.text(0.5, -0.1,
             f"{date_range[0]}/{date_range[1]}/{date_range[2]} - {date_range[3]}/{date_range[4]}/{date_range[5]}",
             horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+
+    # Create a ScalarMappable for colorbar
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])
+
+    # Manually create colorbar
+    cbar = plt.colorbar(sm, ax=ax, ticks=ticker.MaxNLocator(5))
+    cbar.set_label('Your Colorbar Label')  # Set the colorbar label
+
+    # Adds the title
+    plt.title(title)
 
     # Saves the chart as an image
     plt.savefig(f'{title}.png')
--- main.py
+++ main.py
@@ -3,22 +3,17 @@ import Plot
 import DataGrabber
 import geopandas as gpd
 
-colors = [
-    (179/255, 88/255, 6/255),
-    (241/255, 163/255, 64/255),
-    (254/255, 224/255, 182/255),
-    (247/255, 247/255, 247/255),
-    (216/255, 218/255, 235/255),
-    (153/255, 142/255, 195/255),
-    (84/255, 39/255, 136/255)
-]
 
 DateRange = '3', '1', '2024', '3', '31', '2024'
-colors.reverse()
+
 ddTable = DataGrabber.scraper(*DateRange)
 
-WordTableFiller.fillWordTab(ddTable)
+#WordTableFiller.fillWordTab(ddTable)
 
-Plot.plot_departure_map(gpd.read_file('CONUS_CLIMATE_DIVISIONS.shp.zip'), ddTable, 'W_Percent', colors, "Heating Degree Day Percent of Normal (%)", DateRange)
+Plot.plot_departure_map(gpd.read_file('CONUS_CLIMATE_DIVISIONS.shp.zip'), ddTable, 'W_DD', "# of Heating Degree Days", DateRange)
 
-Plot.plot_departure_map(gpd.read_file('CONUS_CLIMATE_DIVISIONS.shp.zip'), ddTable, 'C_Percent', colors, "Cooling Degree Day Percent of Normal (%)", DateRange)
+#Plot.plot_departure_map(gpd.read_file('CONUS_CLIMATE_DIVISIONS.shp.zip'), ddTable, 'C_DD', "# of Cooling Degree Days", DateRange)
+
+Plot.plot_departure_map(gpd.read_file('CONUS_CLIMATE_DIVISIONS.shp.zip'), ddTable, 'W_Departure', "Depature from Heating Degree Day", DateRange)
+
+Plot.plot_departure_map(gpd.read_file('CONUS_CLIMATE_DIVISIONS.shp.zip'), ddTable, 'C_Departure', "Depature from Cooling Degree Day", DateRange)
